63716c567683749288ef68f0bbb72038e75326b4
flokedd <floked224@gmail.com>
2024-08-28 16:59:07 +0300

    draw player on the screen

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
-import pygame
+import pygame, os
+from player import Player
 
 pygame.init()
 screen = pygame.display.set_mode((800, 700))
 clock = pygame.time.Clock()
+player_image = pygame.image.load(os.path.join('assets/ships', 'player-ship.png'))
+
+player = Player(player_image, (35, 42), (screen.get_size()[0]/2-(35/2), screen.get_size()[1]-55))
+player.screen = screen
+
 running = True
 
 while running:
@@ -12,6 +18,8 @@ while running:
             running = False
 
     screen.fill("black")
+
+    player.draw()
 
     pygame.display.flip()
 
--- ship.py
+++ ship.py
@@ -1,8 +1,12 @@
 import pygame
 
 class Ship:
+    screen = None
     def __init__(self, image, size, start_pos) -> None:
-        self.image = image
+        self.image = pygame.transform.scale(image, size)
         self.size = size
         self.start_pos = start_pos
         self.rect = pygame.Rect(start_pos[0], start_pos[1], size[0], size[1])
+
+    def draw(self):
+        self.screen.blit(self.image, self.rect)
