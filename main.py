c944d96ce06d06d9ff71279fb2feb9cfa36e2607
flokedd <floked224@gmail.com>
2024-08-28 19:23:30 +0300

    add movement to player

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ screen = pygame.display.set_mode((800, 700))
 clock = pygame.time.Clock()
 player_image = pygame.image.load(os.path.join('assets/ships', 'player-ship.png'))
 
-player = Player(player_image, (35, 42), (screen.get_size()[0]/2-(35/2), screen.get_size()[1]-55))
+player = Player(player_image, (35, 42), (screen.get_size()[0]/2-(35/2), screen.get_size()[1]-55), 5)
 player.screen = screen
 
 running = True
@@ -18,6 +18,17 @@ while running:
             running = False
 
     screen.fill("black")
+
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_w]:
+        player.move(0, -1)
+    if keys[pygame.K_s]:
+        player.move(0, 1)
+    if keys[pygame.K_a]:
+        player.move(-1, 0)
+    if keys[pygame.K_d]:
+        player.move(1, 0)
 
     player.draw()
 
--- ship.py
+++ ship.py
@@ -2,11 +2,15 @@ import pygame
 
 class Ship:
     screen = None
-    def __init__(self, image, size, start_pos) -> None:
+    def __init__(self, image, size, start_pos, speed) -> None:
         self.image = pygame.transform.scale(image, size)
         self.size = size
         self.start_pos = start_pos
+        self.speed = speed
         self.rect = pygame.Rect(start_pos[0], start_pos[1], size[0], size[1])
+
+    def move(self, direction_x, direction_y):
+        self.rect = self.rect.move(self.speed * direction_x, self.speed * direction_y)
 
     def draw(self):
         self.screen.blit(self.image, self.rect)
