9ba77f588885f4912068e7e4add5a12fb2ca7c2b
flokedd <floked224@gmail.com>
2024-08-28 16:25:28 +0300

    add a rect to Ship

--- ship.py
+++ ship.py
@@ -1,5 +1,8 @@
-
+import pygame
 
 class Ship:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, image, size, start_pos) -> None:
+        self.image = image
+        self.size = size
+        self.start_pos = start_pos
+        self.rect = pygame.Rect(start_pos[0], start_pos[1], size[0], size[1])
